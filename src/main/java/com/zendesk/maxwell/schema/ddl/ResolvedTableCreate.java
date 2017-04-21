7b94b448dd842aacc663fbaea485594a2e3ab02f
BradyPT <brady.auen@webpt.com>
2017-04-21 10:23:09 -0600

    throw table already exists error -> log and move on

--- src/main/java/com/zendesk/maxwell/schema/ddl/ResolvedTableCreate.java
+++ src/main/java/com/zendesk/maxwell/schema/ddl/ResolvedTableCreate.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import com.zendesk.maxwell.schema.*;
 import com.zendesk.maxwell.MaxwellFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ResolvedTableCreate extends ResolvedSchemaChange {
+	static final Logger LOGGER = LoggerFactory.getLogger(ResolvedTableCreate.class);
 	public String database;
 	public String table;
 	public Table def;
@@ -23,8 +26,9 @@ public class ResolvedTableCreate extends ResolvedSchemaChange {
 		Database d = schema.findDatabaseOrThrow(this.database);
 
 		if ( d.hasTable(this.table) )
-			throw new InvalidSchemaError("Unexpectedly asked to create existing table " + this.table);
-
-		d.addTable(this.def);
+			LOGGER.warn("Unexpectedly asked to create existing table " + this.table);
+		else {
+			d.addTable(this.def);
+		}
 	}
 }
